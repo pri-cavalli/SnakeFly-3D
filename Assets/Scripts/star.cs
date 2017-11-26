58f0a98005aad709d1c1f75efa91f94587b42f0d
pri-cavalli <pri-cavalli@hotmai.com>
2017-11-26 13:51:38 -0200

    fixes star

--- Assets/Scripts/star.cs
+++ Assets/Scripts/star.cs
@@ -18,13 +18,20 @@ public class star : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-        time = 10f;
+        time = 20f;
+        sizeX = 20f;
+        sizeZ = 20f;
         StartCoroutine(spawn());
         outOfGame = new Vector3(9999f, 9999f, 9999f);
         isInGame = false;
         text.text = "";
 
         transform.position = outOfGame;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * 15);
     }
 
     private void OnTriggerEnter(Collider collision)
@@ -39,27 +46,33 @@ public class star : MonoBehaviour {
 
     private IEnumerator spawn()
     {
-        string message = "";
-        text.text = message;
-
-        yield return new WaitForSeconds(time);
-        if (isInGame)
+        while (true)
         {
-            transform.position = outOfGame;
-            isInGame = false;
-        } else if (UnityEngine.Random.Range(0,10) < 8)
-        {
-            message = "Star is at the game!\n";
-            isInGame = true;
-            Vector3 newPosition = new Vector3(
-                UnityEngine.Random.Range(-1f * (float)Math.Floor(sizeX / 2) + 1f, (float)Math.Floor(sizeX / 2) - 1f),
-                1.2f,
-                UnityEngine.Random.Range(-1f * (float)Math.Floor(sizeZ / 2) + 1f, (float)Math.Floor(sizeZ / 2) - 1f)
-            );
-            transform.position = newPosition;
+            string message = "";
 
+            yield return new WaitForSeconds(time);
+            Debug.Log("isInGame" + isInGame.ToString());
+            if (isInGame)
+            {
+                Debug.Log("sai" + outOfGame.ToString());
+                transform.position = outOfGame;
+                isInGame = false;
+                message = "";
+
+            }
+            else if (UnityEngine.Random.Range(0, 10) <= 8)
+            {
+                message = "Star is at the game!\n";
+                isInGame = true;
+                Vector3 newPosition = new Vector3(
+                    UnityEngine.Random.Range(-1f * (float)Math.Floor(sizeX / 2) + 1f, (float)Math.Floor(sizeX / 2) - 1f),
+                    1.2f,
+                    UnityEngine.Random.Range(-1f * (float)Math.Floor(sizeZ / 2) + 1f, (float)Math.Floor(sizeZ / 2) - 1f)
+                );
+                Debug.Log("foi" + newPosition.ToString());
+                transform.position = newPosition;
+            }
             text.text = message;
-
         }
     }
 }
